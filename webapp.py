7fa70465051452767fb11897c4e2cecb928d9b70
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-26 13:40:05 +0800

    adding quasar to style typography

--- webapp.py
+++ webapp.py
@@ -4,8 +4,8 @@ import justpy as jp
 def app():
     wp = jp.QuasarPage()
     #creating object for each element
-    wp = jp.QuasarPage(a=wp, text='Analysis of Course Reviews')
-    h1 = jp.QDiv(a=wp, text="These graphs course review analysis")
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These graphs course review analysis")
     return wp
 
 jp.justpy(app)
